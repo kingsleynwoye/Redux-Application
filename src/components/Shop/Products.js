f59cf0c438f0325e89b7ededbf48e0682a4c89c2
nwoyekingsley <kingsleychidubem042@gmail.com.com>
2021-09-29 17:51:16 +0100

    initial commit

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,47 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 1,
+    title: "React native course",
+    description: "The course online",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "Redux",
+    description: "Buiding App with redux",
+  },
+  {
+    id: "p3",
+    price: 5,
+    title: "React",
+    description: "More about react",
+  },
+  {
+    id: "p4",
+    price: 5,
+    title: "Router",
+    description: "Building app with Router",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
